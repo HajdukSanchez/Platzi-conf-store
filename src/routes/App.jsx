c9ccb45ca66ab7fe2cb82aecdda84c570ef2151f
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-06-15 14:02:55 -0500

    :construction: Custom Hook for initial state

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -11,20 +11,26 @@ import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import NotFound from '../containers/NotFound';
 
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
+
 const App = () => {
+  const initialState = useInitialState();
   return (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route exact path="/checkout/information" component={Information} />
-          <Route exact path="/checkout/payment" component={Payment} />
-          <Route exact path="/checkout/success" component={Success} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </Router>
+    <AppContext.Provider value={initialState}>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/checkout/information" component={Information} />
+            <Route exact path="/checkout/payment" component={Payment} />
+            <Route exact path="/checkout/success" component={Success} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </Router>
+    </AppContext.Provider>
   );
 };
 
