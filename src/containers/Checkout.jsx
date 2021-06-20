2035cf3d0a6735f43dbae8b488f4183a9d4e41ce
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-06-20 18:14:06 -0500

    :arrow_up: [Add] React Helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import '../styles/components/Checkout.scss';
 
 import AppContext from '../context/AppContext';
@@ -18,27 +19,32 @@ const Checkout = () => {
   };
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        <h3>{cart.length > 0 ? 'Order List' : 'No items selected yet'}</h3>
-        {cart.map((item) => (
-          <CheckoutItem
-            item={item}
-            handleRemoveFromCart={handleRemoveFromCart}
-          />
-        ))}
+    <>
+      <Helmet>
+        <title>PlatziConf Store | Checkout</title>
+      </Helmet>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          <h3>{cart.length > 0 ? 'Order List' : 'No items selected yet'}</h3>
+          {cart.map((item) => (
+            <CheckoutItem
+              item={item}
+              handleRemoveFromCart={handleRemoveFromCart}
+            />
+          ))}
+        </div>
+        <div className="Checkout-sidebar">
+          <h3>{`Total price: $ ${
+            cart.length > 0 ? handleSumTotal(cart) : '0'
+          }`}</h3>
+          <Link to="/checkout/information">
+            <button type="button" disabled={cart.length <= 0}>
+              Continue order
+            </button>
+          </Link>
+        </div>
       </div>
-      <div className="Checkout-sidebar">
-        <h3>{`Total price: $ ${
-          cart.length > 0 ? handleSumTotal(cart) : '0'
-        }`}</h3>
-        <Link to="/checkout/information">
-          <button type="button" disabled={cart.length <= 0}>
-            Continue order
-          </button>
-        </Link>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import Products from '../components/Products';
 
-const Home = () => <Products />;
+const Home = () => {
+  return (
+    <>
+      <Helmet>
+        <title>PlatziConf Store | Home</title>
+
+        {/* Twitter meta tags */}
+
+        {/* <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@TU_USER" />
+        <meta name="twitter:creator" content="@TU_USER" />
+        <meta name="twitter:title" content="Platzi Conf Store" />
+        <meta name="twitter:description" content="Platzi Conf Store" />
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        /> */}
+
+        {/* Facebook meta tags */}
+
+        {/* <meta property="og:title" content="Platzi Conf Store" />
+        <meta property="og:description" content="Platzi Conf Store" />
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:url" content="platzistore.xyz" />
+        <meta property="og:site_name" content="Platzi Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" /> */}
+      </Helmet>
+      <Products />
+    </>
+  );
+};
 
 export default Home;
